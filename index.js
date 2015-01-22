de1ae95a47763f29f914056274488f7688c9a1fd
Youssef Shahin <yshahin@gmail.com>
2015-01-22 23:55:30 +0200

    read from redis

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-// var redis = null;
-// if (process.env.REDISCLOUD_URL) {
-//   var rtg   = require("url").parse(process.env.REDISCLOUD_URL);
-//   redis = require("redis").createClient(rtg.port, rtg.hostname);
+var redis = null;
+if (process.env.REDISCLOUD_URL) {
+  var rtg   = require("url").parse(process.env.REDISCLOUD_URL);
+  redis = require("redis").createClient(rtg.port, rtg.hostname);
 
-//   redis.auth(rtg.auth.split(":")[1]);
-// } else {
-//   redis = require("redis").createClient();
-// }
+  redis.auth(rtg.auth.split(":")[1]);
+} else {
+  redis = require("redis").createClient();
+}
 
-// redis.on("error", function (err) {
-//   console.log("Error " + err);
-// });
+redis.on("error", function (err) {
+  console.log("Error " + err);
+});
 
 var express = require('express');
 var app = express();
@@ -19,15 +19,16 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(redis.keys("*_*"));
 });
 
 app.get('/add', function(request, response) {
   var timestamp = request.query.timestamp;
   var temp = +request.query.temp / 1000.0;
+  var key = timestamp + "_" + temp
 
-  response.send(timestamp + ': ' + temp);
-  //redis.set("", "");
+  redis.set(key, key);
+  response.send(200);
 });
 
 app.listen(app.get('port'), function() {
