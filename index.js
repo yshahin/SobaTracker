ecbb5a881ebf3ebcdaa638b31e5090c9e08de14c
Youssef Shahin <yshahin@gmail.com>
2015-01-22 23:18:37 +0200

    Enable redis connection test get param reading

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+var redis = require("redis").createClient();
+var client = null;
+
+if (process.env.REDISTOGO_URL) {
+  var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+  client = redis.createClient(rtg.port, rtg.hostname);
+
+  redis.auth(rtg.auth.split(":")[1]);
+} else {
+  client = redis.createClient();
+}
+
+redis.on("error", function (err) {
+  console.log("Error " + err);
+});
+
 var express = require('express');
 var app = express();
 
@@ -6,6 +22,16 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
   response.send('Hello World!');
+});
+
+app.get('/add', function(request, response) {
+  var timestamp = req.query.timestamp;
+  var date = new Date(timestamp*1000);
+  var temp = +req.query.temp;
+  temp = temp / 1000.0;
+
+  response.send(date + ': ' + temp);
+  //client.set("", "");
 });
 
 app.listen(app.get('port'), function() {
